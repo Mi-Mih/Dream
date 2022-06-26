49c750ec9f6185af7c398f680c0e84dd1f00ed58
Mi-Mih <92208626+Mi-Mih@users.noreply.github.com>
2022-06-26 18:55:09 +0300

    Update Dream Coach.js

--- Dream Coach.js
+++ Dream Coach.js
@@ -1,12 +1,38 @@
-var ball_possesion = 1;//параметр владения мячом
+var ball_possesion = 5;//параметр владения мячом 0-10
+var counter_attack = 0; // параметр наличия контратак 0-1
+var pace = 5; // темп 0-10
+var long_short_pass = 5; // соотношение числа длинных и коротких пассов
+var long_short_shot = 5; // соотношение числа дальних и ближних ударов
+var pressing = 5; // прессинг
 var t = 0; // параметр времени матча
-var array = []; // массив, куда записывается значения отклонений
+var array = []; // массив, куда записывается значения отклонений нашего зн-я и точного
 var id; // таймер
 
 /* считывание значения параметров с ползунков */
 document.getElementById('ball_possesion').oninput = function(){
 	ball_possesion = this.value;
 }
+
+document.getElementById('counter_attack').oninput = function(){
+	 counter_attack= this.value;
+}
+
+document.getElementById('pace').oninput = function(){
+	 pace = this.value;
+}
+
+document.getElementById('long_short_pass').oninput = function(){
+	long_short_pass = this.value;
+}
+
+document.getElementById('long_short_shot').oninput = function(){
+	long_short_shot = this.value;
+}
+
+document.getElementById('pressing').oninput = function(){
+	pressing = this.value;
+}
+
 
 /*функция расчёта среднего значения*/
 function  average(arr)
@@ -23,14 +49,21 @@ function  average(arr)
 
 /*функция с параметрами команды соперника*/
 function test_team(t){
-	var a = 5;
-	return a * t;
+	let pace_exact = 5;
+	let counter_attack_exact = 0;
+	let long_short_pass_exact = 5;
+	let ball_possesion_exact = 5;
+	let long_short_shot_exact = 5;
+	let pressing_exact = 5;
+	return pace_exact + counter_attack_exact * t 
+		 + long_short_pass_exact * (t ** 2) + ball_possesion_exact * (t**3) 
+		 + long_short_shot_exact * (t**4) + pressing_exact * (t**5);
 }
 /*функция с параметрами команды соперника*/
 
 /*функция сравнения параметров введённых пользователем и эталонных*/
 function compare(){
-	console.log(t);
+	console.log('время '+t);
 	if (t == 0){
 	    array = [];
 	}
@@ -40,8 +73,13 @@ function compare(){
 	}
 	else if(t % 30 != 0){
 		 let exact = test_team(t);
-	     let num = ball_possesion * t;
-	     array.push(Math.abs(exact - num) / exact);
+	     let num = 	Number(pace) + counter_attack * t 
+		 + long_short_pass * (t ** 2) + ball_possesion* (t**3) 
+		 + long_short_shot * (t**4) + pressing* (t**5);
+		 let error = Math.abs(exact - num) / exact;
+	     array.push(error);
+		 
+		 error = 0;
 	}
 	else if(t % 30 == 0 && t != 0 ){
 	    let av = average(array);
