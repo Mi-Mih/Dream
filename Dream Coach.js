540f4f4dd559ca62b1d7c877f62b2bb22babe1fa
Mi-Mih <92208626+Mi-Mih@users.noreply.github.com>
2022-07-17 23:47:38 +0300

    Update Dream Coach.js

--- Dream Coach.js
+++ Dream Coach.js
@@ -11,8 +11,10 @@ var level = 0.1; //уровень сложности
 var our_score = 0; // забитые мячи нашей команды
 var opponent_score = 0; // забитые мячи команды соперника
 var tour = 0; // номер тура
-var calendar = ['Arsenal','Manchester_City','Manchester_United','Chelsea','Liverpool']; // календарь
-var team_list = ['Arsenal','Manchester_City','Manchester_United','Chelsea','Liverpool']; //список команд для формирования их очков
+var calendar = ['Arsenal','Manchester_City','Manchester_United','Chelsea',
+'Liverpool','Tottenham','Real_Madrid','Barcelona','Atletico_Madrid','Bayern','Borussia_Dortmund','PSG','Milan','Juventus','Inter']; // календарь ред
+var team_list = ['Arsenal','Manchester_City','Manchester_United','Chelsea','Liverpool','Tottenham','Real_Madrid',
+'Barcelona','Atletico_Madrid','Bayern','Borussia_Dortmund','PSG','Milan','Juventus','Inter']; //список команд для формирования их очков ред 
 var rand_minute_arr = [];
 /* считывание значения параметров с ползунков */
 document.getElementById('difficulty level').oninput = function(){
@@ -167,7 +169,7 @@ var Manchester_United = {
 
 	
 };
-/*
+
 var Tottenham = {
 	'points': 0,
 	'Kane': 7.7,
@@ -185,7 +187,168 @@ var Tottenham = {
 
 	
 };
-*/
+
+var Real_Madrid = {
+	'points': 0,
+	'Benzema': 7.5,
+	'Vinisius': 7.6,
+	'Asensio': 7.1,
+	'Kroos':7.1,
+	'Casemiro': 7.3,
+	'Modrich':7.1,
+	'Mendy': 7.0,
+	'Alaba':6.7,
+	'Militao': 7.0,
+	'Vaskes': 7.0,
+	'Courtois': 6.7,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
+
+var Barcelona = {
+	'points': 0,
+	'F. Torres': 6.9,
+	'Aubameang': 7.1,
+	'Dembele': 7.7,
+	'Gavi':6.6,
+	'Busguets': 7.0,
+	'F. De Jong':6.9,
+	'Jordi Alba': 7.0,
+	'Eric Garcia':6.8,
+	'Araujo': 6.8,
+	'Dani Alves': 7.1,
+	'Ter Stegen': 6.5,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
+
+var Atletico_Madrid = {
+	'points': 0,
+	'Cunha': 6.6,
+	'Suarez': 6.6,
+	'Lemar': 6.8,
+	'Condogbia':7.2,
+	'De Paul': 6.6,
+	'Fereira Carassco':7.4,
+	'Reinildo': 6.7,
+	'Gimenez':6.5,
+	'Savic': 6.8,
+	'Llorente': 6.6,
+	'Oblak': 6.3,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
+
+var Bayern = {
+	'points': 0,
+	'Lewandowski': 8.0,
+	'Sane': 7.6,
+	'Gnabry': 7.6,
+	'Muller':7.3,
+	'Goretska': 7.1,
+	'Kimmich':7.4,
+	'Davies': 7.3,
+	'Hernandez':7.0,
+	'Upamecano': 7.1,
+	'Pavard': 6.9,
+	'Neuer': 6.6,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
+
+var Borussia_Dortmund = {
+	'points': 0,
+	'Haaland': 7.7,
+	'T. Hazard': 6.7,
+	'Reus':6.7,
+	'Brandt': 7.1,
+	'Dahoud':6.8,
+	'Can': 6.8,
+	'Shulz':6.5,
+	'Zagadou': 6.5,
+	'Akanji': 7.6,
+	'Munier': 6.6,
+	'Kobel': 6.9,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
+
+var PSG = {
+	'points': 0,
+	'Mbappe': 8.0,
+	'Messi': 7.8,
+	'Di Maria':7.3,
+	'Veratti': 7.4,
+	'Wjinaldum':6.3,
+	'Pereira': 7.0,
+	'Mendes':7.1,
+	'Kimpembe': 6.8,
+	'Marquinius': 6.9,
+	'Hakimi': 7.0,
+	'Navas': 6.9,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
+
+var Milan = {
+	'points': 0,
+	'Giroud': 7.2,
+	'Saelemaekers': 6.7,
+	'Leao':7.3,
+	'B. Diaz': 6.7,
+	'Kessie':6.9,
+	'Tonali': 7.2,
+	'L. Hernandez':7.1,
+	'Tomori': 7.0,
+	'Kalulu': 7.0,
+	'Calabria': 6.9,
+	'Mengian': 6.9,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
+
+var Juventus = {
+	'points': 0,
+	'Morata': 6.8,
+	'Dybala': 7.4,
+	'Rabiot':6.7,
+	'Arthur': 6.5,
+	'Locatelli':7.1,
+	'Cuadrado': 7.1,
+	'Alex Sandro':6.7,
+	'Chiellini': 6.9,
+	'Bonucci': 6.5,
+	'Danilo': 6.7,
+	'Szczesny': 6.7,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
+
+var Inter = {
+	'points': 0,
+	'Dzeko': 7.1,
+	'Lautaro Martinez': 7.2,
+	'Perishic':7.2,
+	'Çalhanoglu': 7.3,
+	'Brozovich':7.1,
+	'Barella': 7.0,
+	'Dumfries':7.0,
+	'Bastoni': 6.9,
+	'De Vrij': 6.7,
+	'Skriniar': 6.7,
+	'Handanovich': 6.8,
+	'counter_attack_exact' : 0, 'long_short_shot_exact' : 4 , 'pace_exact' : 5, 'long_short_pass_exact' : 4, 'ball_possesion_exact' : 6, 'pressing_exact' : 7
+
+	
+};
 /*Возможные команды соперника*/
 
 /*Наша команда*/
@@ -207,14 +370,24 @@ var You = {
 };
 /*Наша команда*/
 
-var Progress = {
+var Progress = { // ред
 	'You':0,
 	'Arsenal':0,
-	//'Tottenham':0,
+	'Tottenham':0,
 	'Manchester_City':0,
 	'Manchester_United':0,
 	'Chelsea':0,
 	'Liverpool':0,
+	'Real_Madrid':0,
+	'Barcelona':0,
+	'Atletico_Madrid':0,
+	'Bayern':0,
+	'Borussia_Dortmund':0,
+	'PSG':0,
+	'Milan':0,
+	'Juventus':0,
+	'Inter':0,
+	
 };
 
 
@@ -222,7 +395,7 @@ var Progress = {
 function create_calendar() {
   calendar.sort(() => Math.random() - 0.5);
   for(var y = 0; y < calendar.length; y++) {
-  document.getElementById("c_" + (y + 1)).innerHTML = 'You : ' + calendar[y];
+  document.getElementById("c_" + (y + 1)).innerHTML = 'You : ' + calendar[y].replace("_", " ");
   }
 }
 /* функция составление итогового календаря */
@@ -274,8 +447,9 @@ var items = Object.keys(input_dict).map(function(key) {
 items.sort(function(first, second) {
   return second[1] - first[1];
 });
-let serial = ['first','second','third','four','five','six']; //id названий команд
-let serial_point = ['first_point','second_point','third_point','four_point','five_point','six_point']; //id очков команд
+let serial = ['first','second','third','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fiveteen','sixteen']; //id названий команд ред
+let serial_point = ['first_point','second_point','third_point','four_point','five_point','six_point',
+'seven_point','eight_point','nine_point','ten_point','eleven_point','twelve_point','thirteen_point','fourteen_point','fiveteen_point','sixteen_point']; //id очков команд ред
 // заполнение турнирной таблицы
 for(var j = 0; j < serial.length; j++) {
 	 document.getElementById(serial[j]).innerHTML = items[j][0].replace("_", " ");
